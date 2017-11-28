10ba49199f354450c2e24bd922a77a792ddfeef9
MFABBRI <MFABBRI@LIT003644.corp.capgemini.com>
2017-11-28 12:42:59 +0100

    hh

--- batch-commons/src/main/java/com/capgemini/batch/commons/launchers/AppUtils.java
+++ batch-commons/src/main/java/com/capgemini/batch/commons/launchers/AppUtils.java
@@ -1,5 +1,9 @@
 package com.capgemini.batch.commons.launchers;
 
+import java.util.Properties;
+
+import javax.annotation.Resource;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.Job;
@@ -8,10 +12,14 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AppUtils {
 	protected Log logger = LogFactory.getLog(AppUtils.class);
 
+	@Resource(name = "fileApplicationProperties")
+	private Properties fileApplicationProperties;
 
 	public void launch(String[] springConfig, String jobName) {
 
@@ -30,6 +38,8 @@ public class AppUtils {
 			
 			JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
 			Job job = (Job) context.getBean(jobName);
+			
+			logger.debug(fileApplicationProperties);
 			
 			JobExecution execution = jobLauncher.run(job, new JobParameters());
 			logger.info("Exit Status : " + execution.getStatus());
