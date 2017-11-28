129ea22163d234966ec0128e97b6c74580ad9244
MFABBRI <MFABBRI@LIT003644.corp.capgemini.com>
2017-11-28 11:17:54 +0100

    Startup

---
+++ batch-prova/src/main/java/com/capgemini/batch/prova/launchers/App.java
@@ -0,0 +1,20 @@
+package com.capgemini.batch.prova.launchers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.capgemini.batch.commons.launchers.AppUtils;
+
+public class App {
+	protected static Log logger = LogFactory.getLog(App.class);
+
+	public static void main(String[] args) {
+
+		// lista dei context soring in input
+		String[] springConfig = { "com-capgemini-batch-prova-context.xml" };
+
+		// nome del job da eseguire
+		new AppUtils().launch(springConfig, "reportJob");
+
+	}
+}
